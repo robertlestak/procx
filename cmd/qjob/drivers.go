f710af20800b1f99a82eeb184497a838d98a5f5a
robertlestak <robert.lestak@umusic.com>
2022-07-30 19:46:29 -0700

    slight main refactor

---
+++ cmd/qjob/drivers.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/robertlestak/qjob/pkg/qjob"
+	log "github.com/sirupsen/logrus"
+)
+
+func initAWSDriver(j *qjob.QJob) {
+	if flagSQSQueueURL != nil && *flagSQSQueueURL != "" {
+		j.Driver = &qjob.Driver{
+			Name: qjob.DriverAWSSQS,
+			AWS: &qjob.DriverAWS{
+				Region:      *flagAWSRegion,
+				RoleARN:     *flagSQSRoleARN,
+				SQSQueueURL: *flagSQSQueueURL,
+			},
+		}
+	}
+}
+
+func initRabbitDriver(j *qjob.QJob) {
+	if flagRabbitMQURL != nil && *flagRabbitMQURL != "" {
+		j.Driver = &qjob.Driver{
+			Name: qjob.DriverRabbit,
+			RabbitMQ: &qjob.DriverRabbitMQ{
+				URL:   *flagRabbitMQURL,
+				Queue: *flagRabbitMQQueue,
+			},
+		}
+	}
+}
+
+func initRedisDriver(j *qjob.QJob) {
+	if flagRedisHost != nil && *flagRedisHost != "" {
+		j.Driver = &qjob.Driver{
+			Name: qjob.DriverName(*flagDriver),
+			Redis: &qjob.DriverRedis{
+				Host:     *flagRedisHost,
+				Port:     *flagRedisPort,
+				Password: *flagRedisPassword,
+				Key:      *flagRedisKey,
+			},
+		}
+	}
+}
+
+func initGCPDriver(j *qjob.QJob) {
+	if flagDriver != nil && qjob.DriverName(*flagDriver) == qjob.DriverGCPPubSub {
+		j.Driver = &qjob.Driver{
+			Name: qjob.DriverGCPPubSub,
+			GCP: &qjob.DriverGCP{
+				ProjectID:        *flagGCPProjectID,
+				SubscriptionName: *flagGCPSubscription,
+			},
+		}
+	}
+}
+
+func initMongoDriver(j *qjob.QJob) error {
+	if flagDriver != nil && qjob.DriverName(*flagDriver) == qjob.DriverMongoDB {
+		pv, err := strconv.Atoi(*flagMongoPort)
+		if err != nil {
+			return err
+		}
+		j.Driver = &qjob.Driver{
+			Name: qjob.DriverMongoDB,
+			Mongo: &qjob.DriverMongo{
+				Host:          *flagMongoHost,
+				Port:          pv,
+				User:          *flagMongoUser,
+				Password:      *flagMongoPassword,
+				DBName:        *flagMongoDatabase,
+				Collection:    *flagMongoCollection,
+				RetrieveQuery: flagMongoRetrieveQuery,
+				ClearQuery:    flagMongoClearQuery,
+				FailureQuery:  flagMongoFailQuery,
+			},
+		}
+	}
+	return nil
+}
+
+func initPsqlDriver(j *qjob.QJob) error {
+	if flagDriver != nil && qjob.DriverName(*flagDriver) == qjob.DriverPostgres {
+		pv, err := strconv.Atoi(*flagPsqlPort)
+		if err != nil {
+			return err
+		}
+		var rps []any
+		var cps []any
+		var fps []any
+		if *flagPsqlRetrieveParams != "" {
+			s := strings.Split(*flagPsqlRetrieveParams, ",")
+			for _, v := range s {
+				rps = append(rps, v)
+			}
+		}
+		if *flagPsqlClearParams != "" {
+			s := strings.Split(*flagPsqlClearParams, ",")
+			for _, v := range s {
+				cps = append(cps, v)
+			}
+		}
+		if *flagPsqlFailParams != "" {
+			s := strings.Split(*flagPsqlFailParams, ",")
+			for _, v := range s {
+				fps = append(fps, v)
+			}
+		}
+		driver := &qjob.DriverPsql{
+			Host:     *flagPsqlHost,
+			Port:     pv,
+			User:     *flagPsqlUser,
+			Password: *flagPsqlPassword,
+			DBName:   *flagPsqlDatabase,
+			SSLMode:  *flagPsqlSSLMode,
+		}
+		if *flagPsqlQueryKey {
+			driver.QueryReturnsKey = flagPsqlQueryKey
+		}
+		if *flagPsqlRetrieveQuery != "" {
+			rq := &qjob.SqlQuery{
+				Query:  *flagPsqlRetrieveQuery,
+				Params: rps,
+			}
+			driver.RetrieveQuery = rq
+		}
+		if *flagPsqlClearQuery != "" {
+			cq := &qjob.SqlQuery{
+				Query:  *flagPsqlClearQuery,
+				Params: cps,
+			}
+			driver.ClearQuery = cq
+		}
+		if *flagPsqlFailQuery != "" {
+			fq := &qjob.SqlQuery{
+				Query:  *flagPsqlFailQuery,
+				Params: fps,
+			}
+			driver.FailureQuery = fq
+		}
+
+		j.Driver = &qjob.Driver{
+			Name: qjob.DriverPostgres,
+			Psql: driver,
+		}
+	}
+	return nil
+}
+
+func initMysqlDriver(j *qjob.QJob) error {
+	if flagDriver != nil && qjob.DriverName(*flagDriver) == qjob.DriverMySQL {
+		pv, err := strconv.Atoi(*flagMysqlPort)
+		if err != nil {
+			return err
+		}
+		var rps []any
+		var cps []any
+		var fps []any
+		if *flagMysqlRetrieveParams != "" {
+			s := strings.Split(*flagMysqlRetrieveParams, ",")
+			for _, v := range s {
+				rps = append(rps, v)
+			}
+		}
+		if *flagMysqlClearParams != "" {
+			s := strings.Split(*flagMysqlClearParams, ",")
+			for _, v := range s {
+				cps = append(cps, v)
+			}
+		}
+		if *flagMysqlFailParams != "" {
+			s := strings.Split(*flagMysqlFailParams, ",")
+			for _, v := range s {
+				fps = append(fps, v)
+			}
+		}
+		driver := &qjob.DriverMysql{
+			Host:     *flagMysqlHost,
+			Port:     pv,
+			User:     *flagMysqlUser,
+			Password: *flagMysqlPassword,
+			DBName:   *flagMysqlDatabase,
+		}
+		if *flagMysqlQueryKey {
+			driver.QueryReturnsKey = flagMysqlQueryKey
+		}
+		if *flagMysqlRetrieveQuery != "" {
+			rq := &qjob.SqlQuery{
+				Query:  *flagMysqlRetrieveQuery,
+				Params: rps,
+			}
+			driver.RetrieveQuery = rq
+		}
+		if *flagMysqlClearQuery != "" {
+			cq := &qjob.SqlQuery{
+				Query:  *flagMysqlClearQuery,
+				Params: cps,
+			}
+			driver.ClearQuery = cq
+		}
+		if *flagMysqlFailQuery != "" {
+			fq := &qjob.SqlQuery{
+				Query:  *flagMysqlFailQuery,
+				Params: fps,
+			}
+			driver.FailureQuery = fq
+		}
+
+		j.Driver = &qjob.Driver{
+			Name:  qjob.DriverMySQL,
+			Mysql: driver,
+		}
+	}
+	return nil
+}
+
+func initDriver(j *qjob.QJob) (*qjob.QJob, error) {
+	l := log.WithFields(log.Fields{
+		"app": "qjob",
+	})
+	l.Debug("starting")
+	if flagSQSQueueURL != nil && *flagSQSQueueURL != "" {
+		initAWSDriver(j)
+	}
+	if flagRabbitMQURL != nil && *flagRabbitMQURL != "" {
+		initRabbitDriver(j)
+	}
+	if flagRedisHost != nil && *flagRedisHost != "" {
+		initRedisDriver(j)
+	}
+	if flagDriver != nil && qjob.DriverName(*flagDriver) == qjob.DriverGCPPubSub {
+		initGCPDriver(j)
+	}
+	if flagDriver != nil && qjob.DriverName(*flagDriver) == qjob.DriverPostgres {
+		if err := initPsqlDriver(j); err != nil {
+			return nil, err
+		}
+	}
+	if flagDriver != nil && qjob.DriverName(*flagDriver) == qjob.DriverMySQL {
+		if err := initMysqlDriver(j); err != nil {
+			return nil, err
+		}
+	}
+	if flagDriver != nil && qjob.DriverName(*flagDriver) == qjob.DriverMongoDB {
+		if err := initMongoDriver(j); err != nil {
+			return nil, err
+		}
+	}
+	l.Debug("exited")
+	return j, nil
+}
---
+++ cmd/qjob/env.go
@@ -0,0 +1,253 @@
+package main
+
+import "os"
+
+func parseAWSFlags() {
+	if os.Getenv("QJOB_AWS_REGION") != "" {
+		r := os.Getenv("QJOB_AWS_REGION")
+		flagAWSRegion = &r
+	}
+	if os.Getenv("QJOB_AWS_SQS_ROLE_ARN") != "" {
+		r := os.Getenv("QJOB_AWS_SQS_ROLE_ARN")
+		flagSQSRoleARN = &r
+	}
+	if os.Getenv("QJOB_AWS_SQS_QUEUE_URL") != "" {
+		r := os.Getenv("QJOB_AWS_SQS_QUEUE_URL")
+		flagSQSQueueURL = &r
+	}
+	if os.Getenv("QJOB_AWS_LOAD_CONFIG") != "" || os.Getenv("AWS_SDK_LOAD_CONFIG") != "" {
+		r := os.Getenv("QJOB_AWS_LOAD_CONFIG")
+		t := r == "true"
+		flagAWSLoadConfig = &t
+	}
+	if *flagAWSLoadConfig {
+		os.Setenv("AWS_SDK_LOAD_CONFIG", "1")
+	}
+}
+
+func parseBaseFlags() {
+	if os.Getenv("QJOB_DRIVER") != "" {
+		d := os.Getenv("QJOB_DRIVER")
+		flagDriver = &d
+	}
+	if os.Getenv("QJOB_HOSTENV") != "" {
+		h := os.Getenv("QJOB_HOSTENV")
+		t := h == "true"
+		flagHostEnv = &t
+	}
+	if os.Getenv("QJOB_PASS_WORK_AS_ARG") != "" {
+		r := os.Getenv("QJOB_PASS_WORK_AS_ARG")
+		t := r == "true"
+		flagPassWorkAsArg = &t
+	}
+	if os.Getenv("QJOB_DAEMON") != "" {
+		r := os.Getenv("QJOB_DAEMON")
+		t := r == "true"
+		flagDaemon = &t
+	}
+}
+
+func parseRabbitFlags() {
+	if os.Getenv("QJOB_RABBITMQ_URL") != "" {
+		r := os.Getenv("QJOB_RABBITMQ_URL")
+		flagRabbitMQURL = &r
+	}
+	if os.Getenv("QJOB_RABBITMQ_QUEUE") != "" {
+		r := os.Getenv("QJOB_RABBITMQ_QUEUE")
+		flagRabbitMQQueue = &r
+	}
+}
+
+func parseRedisFlags() {
+	if os.Getenv("QJOB_REDIS_HOST") != "" {
+		r := os.Getenv("QJOB_REDIS_HOST")
+		flagRedisHost = &r
+	}
+	if os.Getenv("QJOB_REDIS_PORT") != "" {
+		r := os.Getenv("QJOB_REDIS_PORT")
+		flagRedisPort = &r
+	}
+	if os.Getenv("QJOB_REDIS_PASSWORD") != "" {
+		r := os.Getenv("QJOB_REDIS_PASSWORD")
+		flagRedisPassword = &r
+	}
+	if os.Getenv("QJOB_REDIS_KEY") != "" {
+		r := os.Getenv("QJOB_REDIS_KEY")
+		flagRedisKey = &r
+	}
+}
+
+func parseGCPFlags() {
+	if os.Getenv("QJOB_GCP_PROJECT_ID") != "" {
+		r := os.Getenv("QJOB_GCP_PROJECT_ID")
+		flagGCPProjectID = &r
+	}
+	if os.Getenv("QJOB_GCP_SUBSCRIPTION") != "" {
+		r := os.Getenv("QJOB_GCP_SUBSCRIPTION")
+		flagGCPSubscription = &r
+	}
+}
+
+func parsePsqlFlags() {
+
+	if os.Getenv("QJOB_PSQL_HOST") != "" {
+		r := os.Getenv("QJOB_PSQL_HOST")
+		flagPsqlHost = &r
+	}
+	if os.Getenv("QJOB_PSQL_PORT") != "" {
+		r := os.Getenv("QJOB_PSQL_PORT")
+		flagPsqlPort = &r
+	}
+	if os.Getenv("QJOB_PSQL_USER") != "" {
+		r := os.Getenv("QJOB_PSQL_USER")
+		flagPsqlUser = &r
+	}
+	if os.Getenv("QJOB_PSQL_PASSWORD") != "" {
+		r := os.Getenv("QJOB_PSQL_PASSWORD")
+		flagPsqlPassword = &r
+	}
+	if os.Getenv("QJOB_PSQL_DATABASE") != "" {
+		r := os.Getenv("QJOB_PSQL_DATABASE")
+		flagPsqlDatabase = &r
+	}
+	if os.Getenv("QJOB_PSQL_SSL_MODE") != "" {
+		r := os.Getenv("QJOB_PSQL_SSL_MODE")
+		flagPsqlSSLMode = &r
+	}
+	if os.Getenv("QJOB_PSQL_RETRIEVE_QUERY") != "" {
+		r := os.Getenv("QJOB_PSQL_RETRIEVE_QUERY")
+		flagPsqlRetrieveQuery = &r
+	}
+	if os.Getenv("QJOB_PSQL_CLEAR_QUERY") != "" {
+		r := os.Getenv("QJOB_PSQL_CLEAR_QUERY")
+		flagPsqlClearQuery = &r
+	}
+	if os.Getenv("QJOB_PSQL_FAIL_QUERY") != "" {
+		r := os.Getenv("QJOB_PSQL_FAIL_QUERY")
+		flagPsqlFailQuery = &r
+	}
+	if os.Getenv("QJOB_PSQL_RETRIEVE_PARAMS") != "" {
+		r := os.Getenv("QJOB_PSQL_RETRIEVE_PARAMS")
+		flagPsqlRetrieveParams = &r
+	}
+	if os.Getenv("QJOB_PSQL_CLEAR_PARAMS") != "" {
+		r := os.Getenv("QJOB_PSQL_CLEAR_PARAMS")
+		flagPsqlClearParams = &r
+	}
+	if os.Getenv("QJOB_PSQL_FAIL_PARAMS") != "" {
+		r := os.Getenv("QJOB_PSQL_FAIL_PARAMS")
+		flagPsqlFailParams = &r
+	}
+	if os.Getenv("QJOB_PSQL_QUERY_KEY") != "" {
+		r := os.Getenv("QJOB_PSQL_QUERY_KEY")
+		t := r == "true"
+		flagPsqlQueryKey = &t
+	}
+}
+
+func parseMongoFlags() {
+	if os.Getenv("QJOB_MONGO_HOST") != "" {
+		r := os.Getenv("QJOB_MONGO_HOST")
+		flagMongoHost = &r
+	}
+	if os.Getenv("QJOB_MONGO_PORT") != "" {
+		r := os.Getenv("QJOB_MONGO_PORT")
+		flagMongoPort = &r
+	}
+	if os.Getenv("QJOB_MONGO_USER") != "" {
+		r := os.Getenv("QJOB_MONGO_USER")
+		flagMongoUser = &r
+	}
+	if os.Getenv("QJOB_MONGO_PASSWORD") != "" {
+		r := os.Getenv("QJOB_MONGO_PASSWORD")
+		flagMongoPassword = &r
+	}
+	if os.Getenv("QJOB_MONGO_DATABASE") != "" {
+		r := os.Getenv("QJOB_MONGO_DATABASE")
+		flagMongoDatabase = &r
+	}
+	if os.Getenv("QJOB_MONGO_RETRIEVE_QUERY") != "" {
+		r := os.Getenv("QJOB_MONGO_RETRIEVE_QUERY")
+		flagMongoRetrieveQuery = &r
+	}
+	if os.Getenv("QJOB_MONGO_CLEAR_QUERY") != "" {
+		r := os.Getenv("QJOB_MONGO_CLEAR_QUERY")
+		flagMongoClearQuery = &r
+	}
+	if os.Getenv("QJOB_MONGO_FAIL_QUERY") != "" {
+		r := os.Getenv("QJOB_MONGO_FAIL_QUERY")
+		flagMongoFailQuery = &r
+	}
+	if os.Getenv("QJOB_MONGO_COLLECTION") != "" {
+		r := os.Getenv("QJOB_MONGO_COLLECTION")
+		flagMongoCollection = &r
+	}
+}
+
+func parseMysqlFlags() {
+
+	if os.Getenv("QJOB_MYSQL_HOST") != "" {
+		r := os.Getenv("QJOB_MYSQL_HOST")
+		flagPsqlHost = &r
+	}
+	if os.Getenv("QJOB_MYSQL_PORT") != "" {
+		r := os.Getenv("QJOB_MYSQL_PORT")
+		flagPsqlPort = &r
+	}
+	if os.Getenv("QJOB_MYSQL_USER") != "" {
+		r := os.Getenv("QJOB_MYSQL_USER")
+		flagPsqlUser = &r
+	}
+	if os.Getenv("QJOB_MYSQL_PASSWORD") != "" {
+		r := os.Getenv("QJOB_MYSQL_PASSWORD")
+		flagPsqlPassword = &r
+	}
+	if os.Getenv("QJOB_MYSQL_DATABASE") != "" {
+		r := os.Getenv("QJOB_MYSQL_DATABASE")
+		flagPsqlDatabase = &r
+	}
+	if os.Getenv("QJOB_MYSQL_SSL_MODE") != "" {
+		r := os.Getenv("QJOB_MYSQL_SSL_MODE")
+		flagPsqlSSLMode = &r
+	}
+	if os.Getenv("QJOB_MYSQL_RETRIEVE_QUERY") != "" {
+		r := os.Getenv("QJOB_MYSQL_RETRIEVE_QUERY")
+		flagPsqlRetrieveQuery = &r
+	}
+	if os.Getenv("QJOB_MYSQL_CLEAR_QUERY") != "" {
+		r := os.Getenv("QJOB_MYSQL_CLEAR_QUERY")
+		flagPsqlClearQuery = &r
+	}
+	if os.Getenv("QJOB_MYSQL_FAIL_QUERY") != "" {
+		r := os.Getenv("QJOB_MYSQL_FAIL_QUERY")
+		flagPsqlFailQuery = &r
+	}
+	if os.Getenv("QJOB_MYSQL_RETRIEVE_PARAMS") != "" {
+		r := os.Getenv("QJOB_MYSQL_RETRIEVE_PARAMS")
+		flagPsqlRetrieveParams = &r
+	}
+	if os.Getenv("QJOB_MYSQL_CLEAR_PARAMS") != "" {
+		r := os.Getenv("QJOB_MYSQL_CLEAR_PARAMS")
+		flagPsqlClearParams = &r
+	}
+	if os.Getenv("QJOB_MYSQL_FAIL_PARAMS") != "" {
+		r := os.Getenv("QJOB_MYSQL_FAIL_PARAMS")
+		flagPsqlFailParams = &r
+	}
+	if os.Getenv("QJOB_MYSQL_QUERY_KEY") != "" {
+		r := os.Getenv("QJOB_MYSQL_QUERY_KEY")
+		t := r == "true"
+		flagPsqlQueryKey = &t
+	}
+}
+
+func parseEnvToFlags() {
+	parseBaseFlags()
+	parseAWSFlags()
+	parseRabbitFlags()
+	parseRedisFlags()
+	parseGCPFlags()
+	parsePsqlFlags()
+	parseMongoFlags()
+	parseMysqlFlags()
+}
